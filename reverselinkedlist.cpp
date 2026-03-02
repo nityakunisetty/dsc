4e4a7a592936e085f7ebffb8aa7455c9ddff70e4
nityakunisetty <kunisettynitya46@gmail.com>
2026-03-02 06:14:12 +0530

    Add files via upload

---
+++ reverselinkedlist.cpp
@@ -0,0 +1,10 @@
+ListNode* reverseList(ListNode* head) {
+    ListNode *prev = nullptr, *curr = head, *next = nullptr;
+    while (curr) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
